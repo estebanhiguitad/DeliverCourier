7c8223635dbbaaa02131c5db10b38ed697f40572
Esteban Higuita <esteban.higuita@ceiba.com.co>
2022-07-05 17:07:08 -0500

    Modificar test de infrastructure_test.dart

--- lib/infrastructure/test/infrastructure_test.dart
+++ lib/infrastructure/test/infrastructure_test.dart
@@ -7,18 +7,17 @@ import 'object_box_env/test_env.dart';
 void main() {
   late TestEnv env;
   late Store store;
-  late Box<CustomerEntity> box;
 
   setUp(() {
     env = TestEnv('box');
-    box = env.box<CustomerEntity>();
     store = env.store;
   });
 
   tearDown(() => {env.closeAndDelete()});
 
   test('adds one to input values', () {
-    final customer = box.put(CustomerEntity('Esteban'));
+    final box = store.box<CustomerEntity>();
+    final customer = env.box<CustomerEntity>().put(CustomerEntity('Esteban'));
     var result = box.get(customer)!.name;
     expect(result, "Esteban");
   });
