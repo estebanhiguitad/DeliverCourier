7431345c8d4c073e50d86cb5cb4084060b9b1f59
acastillop96 <adrian.castillo@ceiba.com.co>
2022-07-07 16:16:41 -0500

    Se confirma inicio de capa presentation

---
+++ lib/presentation/lib/detail_order/detail_order_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailOrderScreen extends StatelessWidget {
+  const DetailOrderScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentation/lib/list_orders/list_orders_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ListOrdersScreen extends StatelessWidget {
+  const ListOrdersScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
