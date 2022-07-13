2cfb54f80d5189a10fa5a36bba508e993a5e14cb
Esteban Higuita <esteban.higuita@ceiba.com.co>
2022-07-13 15:28:39 -0500

    Agregar pantalla vacía para órdenes

--- lib/delivery_app.dart
+++ lib/delivery_app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'constants.dart';
+
 class DeliveryApp extends StatelessWidget {
   const DeliveryApp({Key? key}) : super(key: key);
 
@@ -7,13 +9,31 @@ class DeliveryApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Delivery'),
+        title: const Text(nameApp),
       ),
       body: SafeArea(
-        child: ElevatedButton(
-          onPressed: () {},
-          child: const Text('Delivery'),
+        child: Expanded(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Image(
+                  image: AssetImage('assets/empty_placeholder.png'),
+                  width: 150,
+                ),
+                Text(
+                  "No tienes pedidos",
+                  style: Theme.of(context).textTheme.headline5,
+                )
+              ],
+            ),
+          ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
