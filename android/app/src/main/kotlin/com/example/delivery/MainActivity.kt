1292717e5c46cec6fdefe1c24e226d78882234dc
Esteban Higuita <esteban.higuita@ceiba.com.co>
2022-07-13 14:08:12 -0500

    Configurar splash screen para android

--- android/app/src/main/kotlin/com/example/delivery/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/delivery/MainActivity.kt
@@ -1,6 +1,29 @@
 package com.example.delivery
 
+import android.os.Build
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import io.flutter.embedding.android.FlutterActivity
+import kotlinx.coroutines.coroutineScope
+import kotlin.coroutines.coroutineContext
+import kotlin.coroutines.suspendCoroutine
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Aligns the Flutter view vertically with the window.
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            // Disable the Android splash screen fade out animation to avoid
+            // a flicker before the similar frame is drawn in Flutter.
+            splashScreen.setOnExitAnimationListener { splashScreenView ->
+                splashScreenView.remove()
+            }
+        }
+
+        super.onCreate(savedInstanceState)
+    }
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,9 +1,5 @@
-import 'package:delivery/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'delivery_app.dart';
 
-routes(BuildContext context) => {
-      '/': (context) => const SplashScreen(),
-      'orders-list': (context) => const DeliveryApp()
-    };
+routes(BuildContext context) => {'/': (context) => const DeliveryApp()};
