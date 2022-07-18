635ab8d67edc6d3d1e75ddaefc4f12c040ff036a
acastillop96 <adrian.castillo@ceiba.com.co>
2022-07-18 18:41:09 -0500

    Se implementa base de capa presentacion

--- lib/presentation/list_orders/list_orders_screen.dart
+++ lib/presentation/list_orders/list_orders_screen.dart
@@ -1,4 +1,4 @@
-import 'package:delivery/presentation/list_orders/list_order_view.dart';
+import 'package:delivery/presentation/list_orders/list_orders_view.dart';
 import 'package:flutter/material.dart';
 
 class ListOrdersScreen extends StatelessWidget {
